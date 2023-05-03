5b84454225e31420f82fe83e5d2a67f0653d83a1
Debarghya Datta <ujan.dattaa@gmail.com>
2023-05-03 19:01:57 +0530

    fix

--- utils/init.js
+++ utils/init.js
@@ -5,8 +5,8 @@ const unhandled = require('cli-handle-unhandled');
 module.exports = ({ clear = true }) => {
 	unhandled();
 	welcome({
-		title: `imcrypt`,
-		tagLine: `by theninza`,
+		title: `picrypt`,
+		tagLine: `by Zenia Banerjee, Shrayosee Roy and Neha Prasad`,
 		description: pkg.description,
 		version: pkg.version,
 		bgColor: '#36BB09',
